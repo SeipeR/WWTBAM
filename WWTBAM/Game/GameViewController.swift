e77295eb45d058fe17ba323e7244fb2826ce99cb
SeipeR <se1per@yandex.ru>
2021-08-16 14:47:48 +0300

    Add screen layout to the game screen

---
+++ WWTBAM/Game/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  WWTBAM
+//
+//  Created by Дамир Доронкин on 16.08.2021.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBAction func firstAnswerAction(_ sender: Any) {
+    }
+    @IBAction func secondAnswerAction(_ sender: Any) {
+    }
+    @IBAction func thirdAnswerAction(_ sender: Any) {
+    }
+    @IBAction func fourthAnswerAction(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- WWTBAM/Game/MainViewController.swift
+++ WWTBAM/Game/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  WWTBAM
 //
 //  Created by Дамир Доронкин on 16.08.2021.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
