030e50859cbb6ab5ba2b7c953d99b9a15b607897
SeipeR <se1per@yandex.ru>
2021-08-16 14:08:35 +0300

    Add question structure

---
+++ WWTBAM/Objects/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  WWTBAM
+//
+//  Created by Дамир Доронкин on 16.08.2021.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let answers: [String]
+    let correctAnswer: String
+}
